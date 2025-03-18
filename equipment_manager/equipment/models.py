823e34a9a9f27d4b80a151957dffcc408360ae09
Артём Коптелов <koptelovu@gmail.com>
2025-03-18 14:05:00 +0500

    modell corrected, venv added

--- equipment_manager/equipment/models.py
+++ equipment_manager/equipment/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 class Equipment(models.Model):
     name = models.CharField(max_length=100)
-    serial_number = models.CharField(max_length=50, unique=True)
+    article = models.DecimalField(max_digits=10, decimal_places=1, default='0')
+    serial_number = models.DecimalField(max_digits=10, decimal_places=1, default='0', unique = True)
     status = models.CharField(max_length=20, choices=[
         ('active', 'Active'),
         ('inactive', 'Inactive'),
         ('under_repair', 'Under Repair')
     ])
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return self.name
