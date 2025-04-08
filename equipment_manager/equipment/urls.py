f155c5b6f10ad80dc932e809874b71a9c0baffd4
Natasha <you@example.com>
2025-04-08 13:45:37 +0500

    AZOS-30 реализовала метод добавления json

--- equipment_manager/equipment/urls.py
+++ equipment_manager/equipment/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from equipment import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.EquipmentListView.as_view(), name='equipment-list'),
+    path('equipment/', views.handle_equipment, name='equipment-handle'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
